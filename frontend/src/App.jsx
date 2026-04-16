5f377577c41d9b123bb801902d9c96d1ee4b3141
rzryhn <rizarayhanw@gmail.com>
2026-04-16 14:15:32 +0700

    create layout, header, navbar and search bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
 
 const App = () => {
-  return <div className="text-red-700">App</div>;
+  return (
+    <BrowserRouter>
+      {/* 
+    www.rabit.com/home
+    www.rabit.com/products
+    www.rabit.com/cart
+    */}
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          {/* User layout */}
+        </Route>
+        <Route>{/* Admin layout */}</Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
