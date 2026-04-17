21492c5ebcc8e9868608555293dc3306d799fd3b
rzryhn <rizarayhanw@gmail.com>
2026-04-17 15:22:33 +0700

    create login, register and order admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
         <Route path="/" element={<UserLayout />}>
           {/* User layout */}
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
         <Route>{/* Admin layout */}</Route>
       </Routes>
